18e8e7ba2536f8d52b266e77027414a85d5e4769
Tim Boring <tim@boring.green>
2019-09-29 19:44:32 -0400

    add initial tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,63 @@
-from click.testing import CliRunner
+import os
 
-from bin import create
+from click.testing import CliRunner
+import pytest
+
+from commands import BASEDIRS
+from commands import BASEFILES
+from commands import create
+
+
+expected_output = """created directory foo
+created directory tests
+created directory src
+created file requirements.txt
+created file README.md
+created file src/api.py
+created file CODEOWNERS
+"""
+
+expected_docker_output = expected_output + 'created file Dockerfile\n'
+expected_models_output = expected_docker_output + 'created file src/models.py\n'
+expected_resources_output = expected_models_output + 'created file src/resources.py\n'
+expected_tox_output = expected_resources_output + 'created file tox.ini\n'
+
 
 
 class TestCli:
 
-    def test_create(self):
+    @pytest.mark.parametrize(
+        'input, output', [
+            (['foo'], expected_output),
+            (['foo', '-d'], expected_docker_output),
+            (['foo', '-d', '-m'], expected_models_output),
+            (['foo', '-d', '-m', '-r'], expected_resources_output),
+            (['foo', '-d', '-m', '-r', '-t'], expected_tox_output)
+        ],
+    )
+    def test_create(self, input, output):
         runner = CliRunner()
         with runner.isolated_filesystem():
-            result = runner.invoke()
+            result = runner.invoke(create, input)
+            assert result.exit_code == 0
+            assert result.stdout == output
+
+            for dir in BASEDIRS:
+                assert os.path.exists(dir)
+
+            for file in BASEFILES:
+                assert os.path.exists(file)
+
+            if '-d' in input:
+                assert os.path.exists('Dockerfile')
+
+            if '-m' in input:
+                assert os.path.exists('src/models.py')
+
+            if '-r' in input:
+                assert os.path.exists('src/resources.py')
+
+            if '-t' in input:
+                assert os.path.exists('tox.ini')
 
 
